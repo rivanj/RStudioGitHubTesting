5e79d95c2f816cbbae6dba20303bb9308db068c6
rivanj <gauravagrawal78@gmail.com>
2020-07-30 17:21:28 -0700

    Recreating local copy after deleting

--- GitHubTesting_1.R
+++ GitHubTesting_1.R
@@ -5,3 +5,5 @@ print("And now it lives on GitHub")
 print("Always remember to push your changes after committing once you are done")
 
 print("Continue testing")
+
+print("Recreating the local copy from GitHub")
